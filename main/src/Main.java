3e2f18c50b46d49fe4d068688824a8ca97301871
jlee59 <lee4300@purdue.edu>
2023-01-21 09:49:31 -0500

    gamelog

--- main/src/Main.java
+++ main/src/Main.java
@@ -1,5 +1,12 @@
+import java.io.FileNotFoundException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+    public static void main(String[] args) throws FileNotFoundException {
+        String file = "WordLibrary.txt";
+        WordLibrary w = new WordLibrary("WordLibrary.txt");
+        System.out.println(w);
+
     }
 }
