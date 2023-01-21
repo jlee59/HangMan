7654158997612184339c1b7605df622a46f8252a
vidit-p <112198253+vidit-p@users.noreply.github.com>
2023-01-21 14:36:16 -0500

    Update Keyboard.java

--- main/src/Keyboard.java
+++ main/src/Keyboard.java
@@ -5,11 +5,46 @@ import java.awt.event.*;
 import java.util.ArrayList;
 import java.util.Random;
 public class Keyboard extends JComponent {
+    static int guess = 0;
+    static WordLibrary wb = new WordLibrary();
+    static String answer = wb.WordLibrary("WordLibrary.txt");
+    static JPanel screen;
+    static JTextField textField0 = new JTextField();
+    static JTextField textField1 = new JTextField();
+    static JTextField textField2 = new JTextField();
+    static JTextField textField3 = new JTextField();
+    static JTextField textField4 = new JTextField();
+    static JTextField textField5 = new JTextField();
+    static JTextField textField6 = new JTextField();
+    static JTextField[] textFields = {textField0, textField1, textField2, textField3, textField4, textField5, textField6};
+
     private static void addButton(JPanel jp, String thing) {
         JButton button = new JButton(thing);
+        button.setActionCommand(thing);
         button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                String buttonChr = e.getActionCommand();
+                Character chr = buttonChr.charAt(0);
+                if (!Main.checkLetterInWord(chr, answer)) {
+                     guess += 1;
+                     button.setBackground(Color.red);
+                     if (!(guess < 5)) {
+                        for (int i = 0; i < answer.length(); i++) {
+                            textFields[i].setText(String.valueOf(answer.charAt(i)).toUpperCase());
+                            
+                        }
+                     }
+                } else {
+                    for (int i = 0; i < answer.length(); i++) {
+                        System.out.println((answer.charAt(i)) + buttonChr.toLowerCase());
+                        char newChar = buttonChr.toLowerCase().charAt(0);
+                        if (newChar == answer.charAt(i)) {
+                            button.setBackground(Color.green);
+                            textFields[i].setText(String.valueOf(answer.charAt(i)).toUpperCase());
+                        }
+                    }
+                }
 
             }
         });
@@ -33,8 +68,11 @@ public class Keyboard extends JComponent {
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         JPanel jp =  keyboard();
         frame.add(jp, BorderLayout.SOUTH);
-        JPanel screen = new JPanel();
-        screen.setLayout(new GridLayout(3,8));
+        screen = new JPanel();
+        screen.setLayout(new GridLayout(0,answer.length()));
+        for (int i = 0; i < answer.length(); i++) {
+            screen.add(textFields[i]);
+        }
         frame.add(screen, BorderLayout.NORTH);
         frame.pack();
         frame.setVisible(true);
