d5851276c47ce046820123a1698aaa133d28d226
Vidit <vidit5904@gmail..com>
2023-01-21 16:54:14 -0500

    New changes

--- main/src/ImageHandler.java
+++ main/src/ImageHandler.java
@@ -17,29 +17,24 @@ public class ImageHandler extends JComponent{
         ImageHandler f = new ImageHandler();
         f.ImageDisplay(1);
     }
-    public void ImageDisplay(int count) throws IOException {
+    public JLabel ImageDisplay(int count) throws IOException {
         File file = null;
         if (count == 1) {
             file = new File("guess 1.jpg");
         } if (count == 2) {
-            file = new File("/Users/josephlee/IdeaProjects/HangMan1/guess 2.jpg");
+            file = new File("guess 2.jpg");
         } if (count == 3) {
-            file = new File("/Users/josephlee/IdeaProjects/HangMan1/guess 3.jpg");
+            file = new File("guess 3.jpg");
         } if (count == 4) {
-            file = new File("/Users/josephlee/IdeaProjects/HangMan1/guess 4.jpg");
+            file = new File("guess 4.jpg");
         } if (count == 5) {
-            file = new File("/Users/josephlee/IdeaProjects/HangMan1/guess 5.jpg");
+            file = new File("guess 5.jpg");
         }
         BufferedImage bi = ImageIO.read(file);
         ImageIcon imageIcon = new ImageIcon(bi);
-        JFrame jFrame = new JFrame();
-        jFrame.setLayout(new FlowLayout());
-        jFrame.setSize(500, 500);
         JLabel jLabel = new JLabel();
         jLabel.setIcon(imageIcon);
-        jFrame.add(jLabel);
-        jFrame.setVisible(true);
-        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        return jLabel;
     }
 
 
--- main/src/Keyboard.java
+++ main/src/Keyboard.java
@@ -1,11 +1,15 @@
 import javax.crypto.Cipher;
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Random;
 public class Keyboard extends JComponent {
-    static int count = 0;
+    static int count = 1;
     static WordLibrary wb = new WordLibrary();
     static String answer = wb.WordLibrary("WordLibrary.txt");
     static JPanel screen;
@@ -18,33 +22,56 @@ public class Keyboard extends JComponent {
     static JTextField textField6 = new JTextField();
     static JTextField[] textFields = {textField0, textField1, textField2, textField3, textField4, textField5, textField6};
 
+    static ArrayList<String> incorrectGuess = new ArrayList<String>();
+    static ArrayList<String> correctGuess = new ArrayList<>();
+    static ArrayList<JButton> buttonList = new ArrayList<>();
+
+    static JFrame frame = new JFrame();
+    static JButton newGame;
     private static void addButton(JPanel jp, String thing) {
         JButton button = new JButton(thing);
         button.setActionCommand(thing);
+        buttonList.add(button);
         button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 String buttonChr = e.getActionCommand();
                 Character chr = buttonChr.charAt(0);
                 if (!Main.checkLetterInWord(chr, answer)) {
+
+                    incorrectGuess.add(String.valueOf(chr));
                     count += 1;
-                    button.setBackground(Color.red);
-                    if (!(count < 5)) {
+                    for (String incorrect : incorrectGuess) {
+                        for (JButton bt : buttonList) {
+                            if (bt.getActionCommand().equals(incorrect)) {
+                                bt.setBackground(Color.red);
+                            }
+                        }
+                    }
+                    if (!(count < 6)) {
                         for (int i = 0; i < answer.length(); i++) {
                             textFields[i].setText(String.valueOf(answer.charAt(i)).toUpperCase());
 
                         }
                     }
                 } else {
+                    correctGuess.add(String.valueOf(chr));
+                    for (String incorrect : incorrectGuess) {
+                        for (JButton bt : buttonList) {
+                            if (bt.getActionCommand().equals(incorrect)) {
+                                bt.setBackground(Color.red);
+                            }
+                        }
+                    }
                     for (int i = 0; i < answer.length(); i++) {
-                        System.out.println((answer.charAt(i)) + buttonChr.toLowerCase());
                         char newChar = buttonChr.toLowerCase().charAt(0);
                         if (newChar == answer.charAt(i)) {
-                            button.setBackground(Color.green);
                             textFields[i].setText(String.valueOf(answer.charAt(i)).toUpperCase());
                         }
                     }
                 }
+                frame.dispose();
+                createGUI();
 
             }
         });
@@ -58,23 +85,71 @@ public class Keyboard extends JComponent {
         jp.setLayout(new GridLayout(3,8));
         for (String character : list) {
             addButton(jp, character);
-            //Add action listener here
         }
         return jp;
     }
 
     public static void createGUI() {
-        JFrame frame = new JFrame("Keyboard");
+        JLabel image = new JLabel();
+        try {
+            ImageHandler ih = new ImageHandler();
+            image = ih.ImageDisplay(count);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "OOPSIE DAISY! SEEMS LIKE YOU ARE OUT OF TRIES!",
+                    "OUT OF TRIES", JOptionPane.ERROR_MESSAGE);
+
+        }
+        JButton newGame = new JButton("New Game");
+        newGame.setActionCommand("newGame");
+        newGame.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                incorrectGuess = new ArrayList<>();
+                correctGuess = new ArrayList<>();
+                count = 1;
+                for (JTextField tf : textFields) {
+                    textField0.setText("");
+                    textField1.setText("");
+                    textField2.setText("");
+                    textField3.setText("");
+                    textField4.setText("");
+                    textField5.setText("");
+                    textField6.setText("");
+                }
+            }
+        });
+        frame.dispose();
+        frame = new JFrame("Keyboard");
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        JPanel game = new JPanel();
+        game.add(newGame);
         JPanel jp =  keyboard();
         frame.add(jp, BorderLayout.SOUTH);
+        frame.add(game, BorderLayout.EAST);
         screen = new JPanel();
         screen.setLayout(new GridLayout(0,answer.length()));
         for (int i = 0; i < answer.length(); i++) {
             screen.add(textFields[i]);
         }
-        frame.add(screen, BorderLayout.NORTH);
+        frame.add(screen);
+        frame.add(image, BorderLayout.NORTH);
         frame.pack();
+        for (String incorrect : incorrectGuess) {
+            for (JButton bt : buttonList) {
+                if (bt.getActionCommand().equals(incorrect)) {
+                    bt.setBackground(Color.red);
+                }
+            }
+        }
+        for (String correct : correctGuess) {
+            for (JButton bt : buttonList) {
+                if (bt.getActionCommand().equals(correct)) {
+                    bt.setBackground(Color.green);
+                }
+            }
+        }
         frame.setVisible(true);
     }
 
